5bd5a9e8c051a7e58d8c524746981b8ddeaa4d76
Sidor_md45@top-academy.ru <Sidor_md45@top-academy.ru>
2024-08-13 15:07:45 +0300

    Move Semantic DONE

--- ForwardList/main.cpp
+++ ForwardList/main.cpp
@@ -16,12 +16,15 @@ public:
 	Element(int Data, Element* pNext = nullptr) :Data(Data), pNext(pNext)
 	{
 		count++;
+#ifdef DEBUG
 		cout << "EConstractor:\t" << this << endl;
+#endif // DEBUG
+
 	}
 	~Element()
 	{
 		count--;
-		cout << "EDestructor:\t" << this << endl;
+		//cout << "EDestructor:\t" << this << endl;
 	}
 	friend class ForwardList;
 };
@@ -39,22 +42,61 @@ public:
 		size = 0;
 		cout << "LConstructor:\t" << this << endl;
 	}
+	ForwardList(const ForwardList& other):ForwardList()
+	{
+		//for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)push_back(Temp->Data);
+		*this = other;
+		cout << "LCopyConstrator: " << this << endl;
+	}
+	ForwardList(ForwardList&& other)noexcept :Head(Head), size(size)
+	{
+		other.Head = nullptr;
+		other.size = 0;
+		cout << "LMoveConstrator: " << this << endl;
+	}
 	~ForwardList()
 	{
+		while (Head)
+		{
+			pop_front();
+		}
+		pop_front();
 		cout << "LDestructor:\t" << this << endl;
 	}
+	//		Operators:
+	ForwardList& operator=(const ForwardList& other)
+	{
+		if (this == &other)return *this;
+		while (Head)
+		{
+			pop_front();
+		}
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)push_back(Temp->Data);
+		cout << "LCopyAssigment: " << this << endl;
+		return *this;
+	}
+	ForwardList& operator=(ForwardList&& other)
+	{
+		if (this == &other)return *this;
+		this->Head = other.Head;
+		this->size = other.size;
 
+		other.Head = nullptr;
+		other.size = 0;
+		cout << "LMoveAssigment: " << this << endl;
+		return *this;
+	}
 	//		Adding elements:
 	void push_front(int Data)
 	{
-		//1. Создаём новый элемент:
-		Element* New = new Element(Data);
+		////1. Создаём новый элемент:
+		//Element* New = new Element(Data);
+		////2. Включаем новый элемент в список:
+		//New->pNext = Head;
+		////3. Переводим голову на новый элемент:
+		//Head = New;
 
-		//2. Включаем новый элемент в список:
-		New->pNext = Head;
-
-		//3. Переводим голову на новый элемент:
-		Head = New;
+		Head = new Element(Data, Head);
 		size++;
 	}
 
@@ -69,10 +111,9 @@ public:
 		}
 
 		//2. Создаём новый элемент:
-		Element* New = new Element(Data);
+		Temp->pNext = new Element(Data);
 
 		//3. Включаем новый элемент в список:
-		Temp->pNext = New;
 		size++;
 	}
 	void insert(int Data, int Index)
@@ -87,16 +128,17 @@ public:
 		}
 
 		//2. Создаем новый элемент
-		Element* New = new Element(Data);
+		Temp->pNext = new Element(Data, Temp->pNext);
 
 		//3. Включаем в список:
-		New->pNext = Temp->pNext;
-		Temp->pNext = New;
+		//New->pNext = Temp->pNext;
+		//Temp->pNext = New;
 		size++;
 	}
 	//			Removibg elements:
 	void pop_front()
 	{
+		if (Head == nullptr)return;
 		Element* erased = Head;
 		Head = Head->pNext;
 		delete erased;
@@ -104,7 +146,7 @@ public:
 	}
 	void pop_back()
 	{
-
+		if (Head == nullptr || Head->pNext == nullptr)return pop_front();
 		Element* Temp = Head;
 		while (Temp->pNext->pNext)
 		{
@@ -135,21 +177,25 @@ public:
 	void print()const
 	{
 		cout << tab << "Head:\t" << Head << endl;
-		Element* Temp = Head;	//Temp- это итератор.
+		/*Element* Temp = Head;	//Temp- это итератор.
 		//Итератор - это указатель, при помощи которого можно перебирать элементы стрктуры данных
 		while (Temp)
 		{
 			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 			Temp = Temp->pNext; //Переход на следующий элемент
-		}
+		}*/
+		for(Element* Temp = Head; Temp; Temp = Temp->pNext)
+			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 		//cout << "Общие количество элементов списка: " << Head->count << endl;
 		cout << "Общие количество элементов списка: " << Element::count << endl;
 		cout << "Количество элементов списка: " << size << endl;
 	}
 };
 
-#define BASE_CHECK
+//#define BASE_CHECK
 //#define COUNT_CHECK
+
+
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -200,6 +246,23 @@ void main()
 	list2.push_back(89);
 	list2.print();
 #endif // COUNT_CHECK
+	cout << "Введите количество элементов: "; cin >> n;
+	ForwardList list;
+	for (int i = 0; i < n; i++)
+	{
+		//if (i % (1024 * 1024)==0)cout << i << tab;
+		list.push_front(rand() % 100);
+		//list.push_back(rand() % 100);
+	}
+	cout << "List filled" << endl;
+	list.print();
 
-
+	ForwardList list2;
+	list2 = (std::move(list));
+	//ForwardList list2;
+	//list2 = list;
+	//list2.print();
+	//ForwardList list3;
+	//ForwardList list4 = list3;
+	
 }
